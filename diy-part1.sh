16d920cb7a3af517aefded8483fed50bd237d5c1
gq32g <75679327+gq32g@users.noreply.github.com>
2022-02-04 22:21:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,11 @@
 
 #echo 'src-git kenzo  https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo  https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git kenzo  https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
 
-git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
+#git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
 
-git clone https://github.com/pali/igmpproxy.git package/lean/igmpproxy
+#git clone https://github.com/pali/igmpproxy.git package/lean/igmpproxy
