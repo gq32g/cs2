bdb65df1cd5bfd40633e60281912fc4d32eafc74
gq32g <75679327+gq32g@users.noreply.github.com>
2022-02-13 09:44:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,6 +27,6 @@
 
 
 
-git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
+#git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
 
-git clone https://github.com/pali/igmpproxy.git package/lean/igmpproxy
+#git clone https://github.com/pali/igmpproxy.git package/lean/igmpproxy.git
